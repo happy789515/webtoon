07e85b1ea438599b8e32fa3f7dd04a428c38e283
SangWon <ondeal@modutech.co.kr>
2023-05-23 14:31:54 +0900

    fromJson

---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = 'https://webtoon-crawler.nomadcoders.workers.dev';
+  final String today = 'today';
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstance = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstance.add(WebtoonModel.from(webtoon));
+      }
+      return webtoonInstance;
+    }
+    throw Error();
+  }
+}
